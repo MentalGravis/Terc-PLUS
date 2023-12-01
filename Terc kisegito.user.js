805752cb8e17063f782aa35647a42375a56018e4
Mental Gravis <74249422+MentalGravis@users.noreply.github.com>
2023-12-01 23:41:00 +0100

    Teljesösszeg_működik

--- Terc kisegito.user.js
+++ Terc kisegito.user.js
@@ -14,13 +14,9 @@
 (function() {
     jQuery(document).ready(function(){
 
-        jQuery("#tu-header > div > div:nth-child(1) > img").hide();
-        //document.getElementById ("myButton").addEventListener ("click", run, false);
+        //jQuery("#tu-header > div > div:nth-child(1) > img").hide();
 
-
-        jQuery("#tu-header > div > div:nth-child(1)").append("<button id=\"myButton\" type=\"button\">Teljes összeg</button>")
-
-    //jQuery("span.x-panel-header-text").append("bruh");
+        //jQuery("#tu-header > div > div:nth-child(1)").append("<button id=\"myButton\" type=\"button\">Teljes összeg</button>")
 
         var run = function(){
             let anyagOssz = Number(document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(5)").textContent.split("\xA0").join(""));
@@ -28,15 +24,13 @@
 
             let teljesOssz = anyagOssz + dijOssz;
 
-            let teljesOsszSzoveg = "<span>Teljes összeg: " + teljesOssz.toLocaleString() + " Ft<\span>";
-
-
+            let teljesOsszSzoveg = "<span><span style=\"font-weight: 400;\">Teljes összeg: </span><span style=\"color:black;\">" + teljesOssz.toLocaleString() + "<\span> Ft<\span>";
 
             document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)").outerHTML = teljesOsszSzoveg;
         }
-        document.getElementById ("myButton").addEventListener ("click", run, false);
-    //if (document.body.contains(jQuery("span.x-panel-header-text"))) {
-    //    run
-    //}
+        //document.getElementById("myButton").addEventListener ("XMLHttpRequest", run, false);
+
+
+        setInterval(run, 500)
     });
 })();
