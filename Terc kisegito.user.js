7c21e71c3a0e10b4ee15ff9d70cb978c0f92236b
Mental Gravis <74249422+MentalGravis@users.noreply.github.com>
2023-12-02 05:41:30 +0100

    Terc-Plus.user.js
    hide menubuttons, push title up

--- Terc kisegito.user.js
+++ Terc kisegito.user.js
@@ -19,18 +19,104 @@
         //jQuery("#tu-header > div > div:nth-child(1)").append("<button id=\"myButton\" type=\"button\">Teljes összeg</button>")
 
         var run = function(){
-            let anyagOssz = Number(document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(5)").textContent.split("\xA0").join(""));
-            let dijOssz = Number(document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(7)").textContent.split("\xA0").join(""));
+            if (!(jQuery('#ext-comp-1077 > b:nth-child(1)').is(':visible'))){
 
-            let teljesOssz = anyagOssz + dijOssz;
+                if (document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)").textContent.includes("Összes munkanem")) {
+                    let anyagOssz = Number(document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(5)").textContent.split("\xA0").join(""));
+                    let dijOssz = Number(document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(7)").textContent.split("\xA0").join(""));
 
-            let teljesOsszSzoveg = "<span><span style=\"font-weight: 400;\">Teljes összeg: </span><span style=\"color:black;\">" + teljesOssz.toLocaleString() + "<\span> Ft<\span>";
+                    let teljesOssz = anyagOssz + dijOssz;
 
-            document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)").outerHTML = teljesOsszSzoveg;
+                    let teljesOsszSzoveg = "<span><span style=\"font-weight: 400;\">Teljes összeg: </span><span style=\"color:black;\">" + teljesOssz.toLocaleString() + "<\span>" + " Ft" + "<\span>";
+
+                    document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)").outerHTML = teljesOsszSzoveg;
+                }
+
+            }
         }
-        //document.getElementById("myButton").addEventListener ("XMLHttpRequest", run, false);
+
+        setInterval(run, 1000)
+
+/*<-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->*/
+
+        var button = document.createElement("Button");
+        button.innerHTML = "Menüsor";
+        button.style = "top:0;left:0;position:absolute;z-index: 9999"
+        document.body.appendChild(button);
 
 
-        setInterval(run, 500)
+
+        if (!(jQuery('#ext-comp-1077 > b:nth-child(1)').is(':visible'))){
+
+            let rightMenuRow = document.querySelectorAll('#maindiv > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td');
+
+        }
+
+        var buttonRearrangement = function(){
+            if (!(jQuery('#ext-comp-1077 > b:nth-child(1)').is(':visible'))){
+                if ((jQuery('.tu-header-cont > div:nth-child(6)').is(':visible')) || (jQuery('.tu-header-cont > div:nth-child(1) > img:nth-child(1)').is(':visible')) || (jQuery('#tu-header-login').is(':visible'))){
+
+                    jQuery('.tu-header-cont > div:nth-child(6)').hide();                        // centerdiv
+                    jQuery('.tu-header-cont > div:nth-child(1) > img:nth-child(1)').hide();     // terc img
+                    jQuery('#tu-header-login').hide();                                          // email
+
+                }
+
+                //let topLeftIconspace = document.querySelector('.tu-header-cont > div:nth-child(1)');
+
+                if(!document.querySelector('.tu-header-cont > div:nth-child(1)').innerHTML.includes('SZERKESZTÉS:')){
+                    let leftMenu = jQuery('#maindiv > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1)');
+                    leftMenu.detach().appendTo('.tu-header-cont > div:nth-child(1)');
+                    if(!document.querySelector('td.x-toolbar-left:nth-child(2)').innerHTML.includes('SZERKESZTÉS:')){
+                        jQuery('td.x-toolbar-left:nth-child(2)').remove();
+                    }
+                }
+
+                if(!(jQuery('.tu-header-cont').css('position') == "relative")){    // relative position for title's parent element so title can go absolute
+                    jQuery('.tu-header-cont').css('position', 'relative');
+                }
+
+                if(!(jQuery('.tu-header-cont > div:nth-child(1)').css('position') == "absolute") || !(jQuery('.tu-header-cont > div:nth-child(1)').css('top') == '50%')){    // absolute position for title, or fucked top title row
+                    jQuery('.tu-header-cont > div:nth-child(1)').css('position', 'absolute').css('top','50%');
+                }
+
+                if(document.querySelector('#maindiv > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1)').align == "right"){
+                    document.querySelector('#maindiv > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1)').align = "left";
+                }
+
+            }
+        }
+
+        let rightMenuRow = function(num){
+            return '#maindiv > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(' + num + ')';
+        }
+
+                //jQuery(rightMenuRow(0).toString()).hide();
+
+        jQuery("button").click(function () {
+
+            jQuery(rightMenuRow(1).toString()).toggle();
+            jQuery(rightMenuRow(2).toString()).toggle();
+            jQuery(rightMenuRow(3).toString()).toggle();
+            jQuery(rightMenuRow(4).toString()).toggle();
+            jQuery(rightMenuRow(5).toString()).toggle();
+            jQuery(rightMenuRow(6).toString()).toggle();
+            jQuery(rightMenuRow(9).toString()).toggle();
+            jQuery(rightMenuRow(10).toString()).toggle();
+            jQuery(rightMenuRow(11).toString()).toggle();
+            jQuery(rightMenuRow(12).toString()).toggle();
+
+            // for (let i = 1; i <= 13; i++) {
+            //     if((i==7) || (i==8) || (i==13)){
+            //         if(jQuery(rightMenuRow(i).toString()).is(':visible')){
+            //             jQuery(rightMenuRow(i).toString()).show()
+            //         }
+            //     }else{
+            //         jQuery(rightMenuRow(i).toString()).toggle();
+            //     }
+            // }
+        });
+
+        setInterval(buttonRearrangement, 1000)
     });
 })();
