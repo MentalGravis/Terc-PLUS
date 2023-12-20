82b78a8f225c9d88987ad81098a38bf926907ff7
Mental Gravis <74249422+MentalGravis@users.noreply.github.com>
2023-12-20 11:24:00 +0100

    repaired teljesösszeg bug

--- Terc.PLUS.user.js
+++ Terc.PLUS.user.js
@@ -17,24 +17,21 @@
 
 /*<---------------------------------- SUM OF MONEY ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->*/
 
-        var szumOfAll = function(){
-            if (!(jQuery('#ext-comp-1077 > b:nth-child(1)').is(':visible'))){
+       var szumOfAll = function(){
+            if (jQuery("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)").text().search("Összes munkanem") >= 0) {
+                let anyagOssz = Number(document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(5)").textContent.split("\xA0").join(""));
+                let dijOssz = Number(document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(7)").textContent.split("\xA0").join(""));
 
-                if (jQuery("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)").text().search("Összes munkanem") >= 0) {
-                    let anyagOssz = Number(document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(5)").textContent.split("\xA0").join(""));
-                    let dijOssz = Number(document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(7)").textContent.split("\xA0").join(""));
+                let teljesOssz = anyagOssz + dijOssz;
 
-                    let teljesOssz = anyagOssz + dijOssz;
+                let teljesOsszSzoveg = "<span><span style=\"font-weight: 400;\">Teljes összeg: </span><span style=\"color:black;\">" + teljesOssz.toLocaleString() + "<\span><span style=\"color:rgb(21 66 166);\">" + " Ft" + "<\span><\span>";
 
-                    let teljesOsszSzoveg = "<span><span style=\"font-weight: 400;\">Teljes összeg: </span><span style=\"color:black;\">" + teljesOssz.toLocaleString() + "<\span><span style=\"color:rgb(21 66 166);\">" + " Ft" + "<\span><\span>";
-
-                    document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)").outerHTML = teljesOsszSzoveg;
-                }
-
+                document.querySelector("#tea-igrid > div:nth-child(1) > span:nth-child(1) > span:nth-child(1)").outerHTML = teljesOsszSzoveg;
             }
+            console.log("fut a szum");
         }
 
-        var szumInterval = setInterval(szumOfAll, 500);
+        //var szumInterval = setInterval(szumOfAll, 500);
 
 /*<---------------------------------- TOGGLE BUTTONS ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->*/
 
@@ -136,11 +133,11 @@
                     selectedAjanlatIndex.push(index);
 
                     let selectedAjanlatIDLookup = (ajanlatokLista + ":nth-child(" + (index+1) + ")" + "> table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(4) > div:nth-child(1) > a").toString();
-                
+
                     let selectedAjanlatID = document.querySelector(selectedAjanlatIDLookup).onclick.toString().match(/(\d(\d?)*\d)/)[0].toString();
 
                     selectedAjanlatLink.push("https://www.etalon.terc.hu/file/dl/" + selectedAjanlatID + "/PDF/2/1/1/1/2/HUF/1/1/1/1/1/2/1/");
-                    
+
                     //window.open(selectedAjanlatLink);
                 }
             });
@@ -186,7 +183,7 @@
 
 
         /*<---------------------------------------- HIDE ON OPEN -------------------------------------------------------------------------------------->*/
- 
+
 
         var currentPage = "";
         var lastPage = "";
@@ -212,6 +209,8 @@
                 jQuery('.tu-header-cont > div:nth-child(1) > img:nth-child(1)').hide();     // terc img
             }
 
+            //var szumInterval;
+
             if ((lastPage == "frontPage") && (currentPage == "innerPage")){             // kívülről befele váltás
                 if (!(jQuery('#ext-comp-1077 > b:nth-child(1)').is(':visible')) && jQuery(rightMenuRow(6).toString()).is(':visible')){
                     jQuery(rightMenuRow(1).toString()).hide();
@@ -234,7 +233,8 @@
 
                 };
 
-                szumInterval
+
+                //szumOfAll;
 
                 massExportButton.style.visibility = "hidden";
                 showHideButton.style.visibility = "visible";
@@ -244,7 +244,7 @@
 
             }
             if ((lastPage == "innerPage") && (currentPage == "frontPage")) {            // belülről kifele váltás
-                clearInterval(szumInterval);
+
                 massExportButton.style.visibility = "visible";
                 showHideButton.style.visibility = "hidden";
                 topKoltsKez.style.padding = "2px 0px";
@@ -253,17 +253,16 @@
                 jQuery('.tu-header-cont > div:nth-child(1) > img:nth-child(1)').hide();     // terc img
             }
 
-
             if ((lastPage == "innerPage") && (currentPage == "innerPage")) {
-                szumInterval;
+                szumOfAll();
             }
-            //console.log("lastPage: " + lastPage + "  ||  currentPage: " + currentPage);
+            // console.log("lastPage: " + lastPage + "  ||  currentPage: " + currentPage);
             lastPage = currentPage;
 
 
         }
 
-        setInterval(freshPage, 1000);
+        setInterval(freshPage, 750);
 
 
     });
